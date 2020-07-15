bcaafaed966fe41be78458593ed45c0b6039c94c
Riley Griffith <rileyjgriffith@gmail.com>
2020-07-15 18:26:11 -0500

    initial

---
+++ Instagram Clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram Clone
+//
+//  Created by user173030 on 7/15/20.
+//  Copyright © 2020 Riley Griffith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
