9677aecc2c4ad55bc7e2b7450ff83a731a2df639
Riley Griffith <rileyjgriffith@gmail.com>
2020-07-16 16:40:14 -0500

    Added sign up page and auto layout constraints

---
+++ Instagram Clone/SignUpViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SignUpViewController.swift
+//  Instagram Clone
+//
+//  Created by user173030 on 7/16/20.
+//  Copyright © 2020 Riley Griffith. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func dismissOnClick(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
