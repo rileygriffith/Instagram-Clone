ae6d197e6c35c195db7aa07138fbfb5392047381
Riley Griffith <rileyjgriffith@gmail.com>
2020-07-20 18:28:42 -0500

    separated each tab view into separate storyboard, tweaked fixes on login page

--- Instagram Clone/SignInViewController.swift
+++ Instagram Clone/SignInViewController.swift
@@ -9,22 +9,14 @@
 import UIKit
 
 class SignInViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        emailTextField.backgroundColor = UIColor.clear
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
