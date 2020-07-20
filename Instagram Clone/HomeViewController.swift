4be9d82c6b2ff1d77f9cba1fecb2f6cb0b404dd8
Riley Griffith <rileyjgriffith@gmail.com>
2020-07-20 16:05:16 -0500

    added all 5 tab bar views among other changes

---
+++ Instagram Clone/HomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HomeViewController.swift
+//  Instagram Clone
+//
+//  Created by user173030 on 7/20/20.
+//  Copyright © 2020 Riley Griffith. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
